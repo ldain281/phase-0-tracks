47399875d2d1611641e2c2b7df5430a07760597f
Leonard Dain <ivetiapaniagua@ivetias-iMac.local>
2017-02-07 22:26:08 -0800

    added puppy characteristics and behavior

---
+++ ruby/puppy.rb
@@ -0,0 +1,26 @@
+# SPECIES ------------------------
+# Puppy energeticus
+
+# CHARACTERISTICS ----------------
+# Weight: varies
+# Friendly: yes
+# Eye count: 2
+# Leg count: 4
+# Ear count: 2
+# Name: varies
+# Fur color: varies
+# Fur length: varies
+# Bark stength: varies
+
+# BEHAVIOR -----------------------
+# Fetch
+# Pee
+# Poop
+# Scamper
+# Bark
+# Growl
+# Knaw
+# Drool
+# Eat
+# Sleep
+
