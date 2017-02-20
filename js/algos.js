3f7501a432d0bbb196760c4932721b3f450645ae
Leonard Dain <ivetiapaniagua@ivetias-iMac.local>
2017-02-20 09:50:58 -0800

    release1 and release2 pending

---
+++ js/algos.js
@@ -0,0 +1,35 @@
+//Release 0: create function that returns longest phrase in array
+//set minimum length variable and longest variable for comparison
+//count length of strings in a loop and
+//compare to other strings and return longest
+
+//var phrase = ["All good soules", "For whom the bell tolls", "Fear not"];
+//var x = 
+phrase = ["All good soules", "For whom the bell tolls", "Fear not"];
+// longPhrase(x);
+//function longPhrase(phrase) {
+
+var longest = 0;
+var longestString = " ";
+for (var i = 0; i < phrase.length; i ++) {
+	if (phrase[i].length > longest) {
+	longest = phrase[i].length;
+	longestString = phrase[i];
+
+	}
+	}
+console.log(longestString);
+
+//Release1 function that takes two objects
+//if objects share at least one key-value pair returns true
+//if objects don't share kv pair, returns false
+//include driver code
+function Valuepair(name)
+
+
+
+//release2: takes integer for length
+//builds and returns array of stings of given length
+//include driver code
+function length(x) {  
+	var }
