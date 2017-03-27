71f1f215ac973e7aaec1a7cb4203c48f921fa851
Leonard Dain <ivetiapaniagua@ivetias-iMac.local>
2017-03-26 22:30:22 -0700

    6.3 review and update

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -9,18 +9,17 @@ class Santa
 	end
 
 	def eat_milk_and_cookies(cookie_type)
-	puts "That was a good #{cookie_type}!"
+	puts "That was a good #{cookie_type} cookie!"
 	end
 #release1 initialize attributes and states
 	def initialize(gender, ethnicity, age)
 	puts "Initializing Santa instance..."
 		@gender = gender
 		@ethnicity = ethnicity
-
 		@age = age
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 	end
-	# release 2
+# release 2: change attribute with a method
 	def celebrate_birthday(age)
 		@age = age + 1
 		p @age
@@ -36,9 +35,9 @@ class Santa
 end
 
 #release 0
-st_nick = Santa.new("female", "Native American", 0)
+st_nick = Santa.new("male", "Latino", 0)
 st_nick.speak
-st_nick.eat_milk_and_cookies("sugar cookie")
+st_nick.eat_milk_and_cookies("peanut butter")
 #release 1 (and Release4)
 santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
@@ -59,8 +58,8 @@ st_nick.get_mad_at("Vixen", 4)
 # Set your new Santa's age to a random number between 0 and 140.
 # No need to store your Santas in a data structure, but your program should print out the attributes of each Santa using the instance methods that give you access to that data.
 #loop to create 100 santas with random gender, ethnicity and age
-  80.times do
+  100.times do
     puts "Creating new Santa"
-    santas << Santa.new(example_genders.sample, example_ethnicities.sample, rand(100))
+    santas << Santa.new(example_genders.sample, example_ethnicities.sample, rand(122))
 end
-
+p santas
