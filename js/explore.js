a717a5c0509d70c6db3ca559ad9726d52d16c85e
Leoanrd Dain <ldain281@gmail.com>
2017-03-28 11:28:47 -0700

    string reversal step1 and 2

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+//create a function that takes a string as a paramater and reverses the string
+//for each string, split the string into characters, reverse the order and join
+
+function reverse("Hello"); {
+
+
+
+}
