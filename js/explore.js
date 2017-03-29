81c0629312833c7afdd7dee5fa8d3773c5b79d82
Leoanrd Dain <ldain281@gmail.com>
2017-03-29 12:54:42 -0700

    reverse string with conditional print

--- js/explore.js
+++ js/explore.js
@@ -1,8 +1,20 @@
 //create a function that takes a string as a paramater and reverses the string
 //for each string, split the string into characters, reverse the order and join
+//print to console
 
-function reverse("Hello"); {
+function reverse(str) {
+var reversedStr = "";
+
+//loop backward reversed inde,x starting with last character of string
+for (var i = str.length - 1; i >=0 ; i--) {
+
+reversedStr += str[i];
+	}
 
 
+}
 
+//driver code
+if (1 == 1) {
+console.log(reverse('painting'));
 }
