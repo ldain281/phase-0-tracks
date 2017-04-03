45159afc8146dc772ab46d8d7ee6905e907d7b2a
Leonard Dain <ivetiapaniagua@ivetias-iMac.local>
2017-04-02 20:58:29 -0700

    functioning game, part complete spec

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -21,39 +21,65 @@ attr_reader :ans_word, :guess_word
 	def initialize(ans_word)
 		@ans_word = ans_word
 		@guess_word = []
-		@num_guesses = 0
 		@guess = []
 		p @ans_word
 	end
-	#this method will calculate number of letters in word and display spaces
+#this method will calculate number of letters in word and display empty spaces
 	def word_status
 		@ans_word.length.times do
-			@guess_word << "_"
+			@guess << "_"
 		end
-		puts "Guesser, your secret word has #{@ans_word.length} characters: #{@guess_word*""}"
+		puts "Guesser, your secret word has #{@ans_word.length} characters: #{@guess*""}"
+	end
+#this method will define the number of guesses the guesser will have
+	def word_length
+		@ans_word.length
 	end
 #this method will take in a guessed letter and place it into the guessed word character array
 	def guess_letter(letter)
 		if @ans_word.include?(letter)
 			i = @ans_word.index(letter)
-			@guess_word.insert(i, letter)
-			@guess_word.pop
-			puts "You are getting closer: #{@guess_word*""}"
+			@guess.insert(i, letter)
+			@guess.pop
+			puts "Good! You are getting closer: #{@guess*""}"
 		else
 			puts "sorry, guess again"
 		end
 	end
 	#this method will compare the guessed word to the answer word
+	def guessed_word(word)
+		if @ans_word == word
+			puts "You are a genious"
+		else
+			puts "guess again"
+			@guess_word.pop
+		end
+	end
 
-	
 end
+# user interface
 puts "Wordsmith, enter your secret word for the Guesser:"
 game1 = Game.new(gets.chomp)
 game1.word_status
-puts "Can you guess a letter in the word?"
-game1.guess_letter(gets.chomp)
+puts "You have #{game1.word_length} guesses to resolve the secret word"
+	x = game1.word_length
+	while x > 0
+puts "Would you like to guess a 'letter' or 'word'?"
+	if "letter" == gets.chomp
+		puts "Enter your letter:"
+		game1.guess_letter(gets.chomp)
+	else
+		puts "Enter your word guess:"
+		game1.guessed_word(gets.chomp)
+	end
+	  x = x-1
+	end
+	if guessed_word(word) == true
+		break
+	else
+		puts "Sorry, you used up all your guesses, you lost HAAAA!!"
+	end
 
 
 
- 	# @ans_word.split(" ")
- 	# @guess_word.split(" ")
+
